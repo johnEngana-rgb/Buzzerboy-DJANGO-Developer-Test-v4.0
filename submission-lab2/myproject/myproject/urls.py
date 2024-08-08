de74d448cc66f7214dd47572fd391772fe2322ca
John Engana <JohnEric.Engana@robinsonsland.com>
2024-08-09 02:56:36 +0800

    worked

--- submission-lab2/myproject/myproject/urls.py
+++ submission-lab2/myproject/myproject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.test, name='home'),
+    # Add paths for other views
 ]
